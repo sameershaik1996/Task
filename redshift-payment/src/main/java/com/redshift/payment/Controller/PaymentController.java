fd6c78a30b2cb8aa119a37aa5b7e0fe5bea3c7f1
sameer <sameershaik03@gmail.com>
2019-03-29 11:12:38 +0530

    hazel cast

--- redshift-payment/src/main/java/com/redshift/payment/Controller/PaymentController.java
+++ redshift-payment/src/main/java/com/redshift/payment/Controller/PaymentController.java
@@ -1,5 +1,6 @@
 package com.redshift.payment.Controller;
 
+import com.hazelcast.core.HazelcastInstance;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.redshift.payment.Data.PaymentRepository;
@@ -7,28 +8,43 @@ import com.redshift.payment.Domain.Payment;
 import com.redshift.payment.RedshiftExceptionHandling;
 import com.redshift.payment.Response.RedshiftResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/payments")
+@CacheConfig(cacheNames = "payments")
 public class PaymentController {
 
     @Autowired
     PaymentRepository _paymentRepository;
 
+    @Autowired
+    private HazelcastInstance instance;
 
 
     @RequestMapping(value = "", method = RequestMethod.POST)
+    @Cacheable(key="#payment.getOrderId()")
     public Payment CreatePayment(@Valid @RequestBody Payment payment) {
-        try {
-            Thread.sleep(5000);
-        }catch (Exception e){}
+
         System.out.println(payment.getOrder_id());
 if(payment.getCardType().equalsIgnoreCase("visa"))
-        return _paymentRepository.save(payment);
+{
+    Payment created_payment = _paymentRepository.save(payment);
+
+    Map<String,Payment> stringStringMap = instance.getMap("payments");
+    System.out.println(stringStringMap);
+    stringStringMap.put("payment-order-"+created_payment.getOrder_id(),created_payment);
+
+    return created_payment;
+
+
+}
 else
     return new Payment();
     }
@@ -37,15 +53,25 @@ else
     public Payment GetPayment(@PathVariable(value="id") Long id){
 
         System.out.println("hi");
+        Map<String,Payment> stringStringMap = instance.getMap("payments");
+        Payment result=stringStringMap.get("payment-order-"+id);
+
         return _paymentRepository.findById(id).orElseThrow(()->new RedshiftExceptionHandling("not found","id",id));
     }
 
 
-
+    @Cacheable(key="#id")
     @RequestMapping(value="/orders/{orderId}",method= RequestMethod.GET)
     public Payment GetPaymentByOrderId(@PathVariable(value="orderId") Long id){
-
-        return _paymentRepository.getPaymentByOrderId(id);
+        Map<String,Payment> stringStringMap = instance.getMap("payments");
+        System.out.println(stringStringMap);
+        Payment result=stringStringMap.get("payment-order-"+id);
+        try{
+            System.out.println(result.getId());
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
+        return result!=null?result:_paymentRepository.getPaymentByOrderId(id);
         //return _paymentRepository.findById(id).orElseThrow(()->new RedshiftExceptionHandling("not found","id",id));
     }
 
--- redshift-payment/src/main/java/com/redshift/payment/RedshiftPaymentApplication.java
+++ redshift-payment/src/main/java/com/redshift/payment/RedshiftPaymentApplication.java
@@ -2,10 +2,12 @@ package com.redshift.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableHystrix
+@EnableCaching
 public class RedshiftPaymentApplication {
 
 	public static void main(String[] args) {
